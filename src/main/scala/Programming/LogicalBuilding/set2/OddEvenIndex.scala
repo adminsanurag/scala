c7472ab675ca86430b53e0b6f1bb49e194d57cd1
Anurag Singh <anurag9707@gmail.com>
2023-04-18 20:55:57 +0530

    new update

---
+++ src/main/scala/Programming/LogicalBuilding/set2/OddEvenIndex.scala
@@ -0,0 +1,35 @@
+package Programming.LogicalBuilding.set2
+
+
+/*import scala.collection.mutable.ListBuffer
+object OddEvenIndex extends App{
+
+  // Read a array of numbers and , print the numbers  which are present in odd and even indexes seperately
+
+  var l= List(7,5,8,93,0,45,23,87,34,12,65)
+  var len =l.length
+
+
+  var eve = new ListBuffer[Int]()
+  var od = new ListBuffer[Int]()
+ for(k<-0 to len)
+  {
+    var i= l(k)
+     if(i%2==0)
+       {
+        eve += i
+       }else
+       {
+         od+= i
+        // print(i+" add on")
+       }
+
+    for(i<- od)
+      {
+        print(s"$i ")
+      }
+   }
+
+
+}
+*/
